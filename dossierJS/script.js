2c2dbcc1fbc6430dd9ecaf29b28d99ee14883522
YNKS971 <dylan.pqt@gmail.com>
2025-06-26 11:35:31 +0200

    base

---
+++ dossierJS/script.js
@@ -0,0 +1,9 @@
+const mot=["savon","devin","gonfler","pieux","Vinaigre","Homard","Mangue","hibou","phoque","iguane","turquoise","bleu","fuchsia","argenté","poitrine","cheveux","gendre","femme","mégaphone"]
+let aleatoire=(Math.floor(Math.random() *(mot.length)))
+let WordToGuess= mot[aleaNumber]
+for (let i=0;i <= mot.length;i++){
+    document.getElementById
+}
+// relier chaque lettre au bouton 
+// nombre d"zqq i
+
