3e69eb351fadd0366d080653f2c8c20e6345aff8
YNKS971 <dylan.pqt@gmail.com>
2025-06-29 21:59:20 +0200

    Base

--- dossierJS/script.js
+++ dossierJS/script.js
@@ -1,9 +1,21 @@
+// On stocke les mots
 const mot=["savon","devin","gonfler","pieux","Vinaigre","Homard","Mangue","hibou","phoque","iguane","turquoise","bleu","fuchsia","argenté","poitrine","cheveux","gendre","femme","mégaphone"]
+// Parmi tout les mots on cree de l'aleatoire
 let aleatoire=(Math.floor(Math.random() *(mot.length)))
-let WordToGuess= mot[aleaNumber]
+// Le mot choisi est: 
+let WordToGuess= mot[aleatoire]
+
 for (let i=0;i <= mot.length;i++){
-    document.getElementById
+    document.getElementById ("secret").innerText+="-"
+    
 }
+
+function lesLettres(){
+
+}
+
+let Chances=10;
+document.getElementById ("vies")
 // relier chaque lettre au bouton 
 // nombre d"zqq i
 
